ab4735bb811fa513c29572bce1d37daa7222ff75
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-29 12:46:28 +0300

    feat: task 3 completed

--- tasks/task3/task3.js
+++ tasks/task3/task3.js
@@ -6,63 +6,79 @@
  * со всей вложенной структурой, в котором ключи объектов заменены на camelCase.
  **/
 
-function toCamelCase(val) {}
+function toCamelCase(val) {
+	const result = Array.isArray(val) ? [] : {};
 
+	for (key in val) {
+		const newKey = key
+			.split("_")
+			.map((item, index) => {
+				if (index === 0) {
+					return item;
+				}
+				const newItem = item.split("");
+				newItem[0] = newItem[0].toUpperCase();
+				return newItem.join("");
+			})
+			.join("");
+
+		result[newKey] =
+			val[key] instanceof Object ? toCamelCase(val[key]) : val[key];
+	}
+
+	return result;
+}
 
 /*------------------*/
 /*    Test cases    */
 /*------------------*/
 
-
 const testcases = [
-  {
-    args: [
-      {
-        simple_prop: 'a',
-        empty_prop: null,
-        empty_obj_prop: {},
-        object_prop: {
-          inside_prop: 3
-        },
-        array_prop: [
-          {
-            inside_array_prop: 'b'
-          },
-          {
-            inside_array_prop: {
-              deep_inside_prop: 'c'
-            }
-          }
-        ]
-      }
-    ],
-    result:
-      {
-        simpleProp: 'a',
-        emptyProp: null,
-        emptyObjProp: {},
-        objectProp: {
-          insideProp: 3
-        },
-        arrayProp: [
-          {
-            insideArrayProp: 'b'
-          },
-          {
-            insideArrayProp: {
-              deepInsideProp: 'c'
-            }
-          }
-        ]
-      }
-  },
-  {
-    args: [
-      {}
-    ],
-    result: {}
-  }
+	{
+		args: [
+			{
+				simple_prop: "a",
+				empty_prop: null,
+				empty_obj_prop: {},
+				object_prop: {
+					inside_prop: 3,
+				},
+				array_prop: [
+					{
+						inside_array_prop: "b",
+					},
+					{
+						inside_array_prop: {
+							deep_inside_prop: "c",
+						},
+					},
+				],
+			},
+		],
+		result: {
+			simpleProp: "a",
+			emptyProp: null,
+			emptyObjProp: {},
+			objectProp: {
+				insideProp: 3,
+			},
+			arrayProp: [
+				{
+					insideArrayProp: "b",
+				},
+				{
+					insideArrayProp: {
+						deepInsideProp: "c",
+					},
+				},
+			],
+		},
+	},
+	{
+		args: [{}],
+		result: {},
+	},
 ];
 
-module.exports['testcases'] = testcases;
-module.exports['solution'] = toCamelCase;
+module.exports["testcases"] = testcases;
+module.exports["solution"] = toCamelCase;
