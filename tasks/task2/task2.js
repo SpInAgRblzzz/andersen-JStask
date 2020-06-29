8aafd78e6c21600a5a7dad119347dfd4c006dd2e
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-06-29 12:10:34 +0300

    feat: task 2 completed

--- tasks/task2/task2.js
+++ tasks/task2/task2.js
@@ -15,33 +15,49 @@
  в порядке следования по маршруту.
  **/
 
-function getRoute(tickets = []) {}
+function getRoute(tickets = []) {
+	const result = [];
+	let len = result.lenght;
 
+	let current = tickets.find(
+		(findElement) =>
+			!tickets.some((someElement) => someElement.to === findElement.from)
+	);
+
+	while (tickets.length !== len) {
+		result.push(current);
+		len = result.length;
+
+		current = tickets.find(
+			(findElement) => findElement.from === current.to
+		);
+	}
+
+	return result;
+}
 
 /*------------------*/
 /*    Test cases    */
 /*------------------*/
 
-
 const testcases = [
-  {
-    args: [
-      [
-        { from: 'London', to: 'Moscow' },
-        { from: 'New-York', to: 'London' },
-        { from: 'Moscow', to: 'SPb' },
-        { from: 'San-Francisco', to: 'New-York' }
-      ]
-    ],
-    result:
-      [
-        { from: 'San-Francisco', to: 'New-York' },
-        { from: 'New-York', to: 'London' },
-        { from: 'London', to: 'Moscow' },
-        { from: 'Moscow', to: 'SPb' }
-      ]
-  }
+	{
+		args: [
+			[
+				{ from: "London", to: "Moscow" },
+				{ from: "New-York", to: "London" },
+				{ from: "Moscow", to: "SPb" },
+				{ from: "San-Francisco", to: "New-York" },
+			],
+		],
+		result: [
+			{ from: "San-Francisco", to: "New-York" },
+			{ from: "New-York", to: "London" },
+			{ from: "London", to: "Moscow" },
+			{ from: "Moscow", to: "SPb" },
+		],
+	},
 ];
 
-module.exports['testcases'] = testcases;
-module.exports['solution'] = getRoute;
+module.exports["testcases"] = testcases;
+module.exports["solution"] = getRoute;
